8f2b75e44a4a5d2b4ff53bf968f7a7eb2275af67
duynhandev0511 <tranduynhan497@gmail.com>
2026-02-22 20:43:41 +0700

    first commit

--- src/components/header.js
+++ src/components/header.js
@@ -15,7 +15,6 @@ const Header = ({ isLoggedIn, userRole, onLoginClick }) => {
           <svg className="me-2" xmlns="http://www.w3.org/2000/svg" height="35" viewBox="0 -960 960 960" fill="#FFFFFF">
             <path d="M480-480q33 0 56.5-23.5T560-560q0-33-23.5-56.5T480-640q-33 0-56.5 23.5T400-560q0 33 23.5 56.5T480-480Zm0 294q122-112 181-203.5T720-552q0-109-69.5-178.5T480-800q-101 0-170.5 69.5T240-552q0 71 59 162.5T480-186Zm0 106Q319-217 239.5-334.5T160-552q0-150 96.5-239T480-880q164 0 260.5 89T837-552q0 127-79.5 244.5T480-80Z"/>
           </svg>
-          BẢN ĐỒ SỐ ẤP 40
         </Navbar.Brand>
 
         <Navbar.Toggle aria-controls="responsive-navbar-nav" className="border-0 bg-white" />
